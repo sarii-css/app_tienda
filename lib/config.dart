fc76dde80d61413a666535e0f0625b529a747f9e
sarii-css <lesliesaraiberistainp23.cb59@dgeti.sems.gob.mx>
2026-06-02 08:30:10 -0600

    cambios ip

--- lib/config.dart
+++ lib/config.dart
@@ -3,9 +3,10 @@ import 'dart:io';
 class Config {
   static String get baseUrl {
     if (Platform.isAndroid) {
-      return "http://10.0.2.2:8080";
-    } else {
+      // camiar entre ip (test en celular) & "http://10.0.2.2:8080" (test en emulador)
       return "http://192.168.0.10:8080";
+    } else {
+      return "http://localhost:8080";
     }
   }
 }
