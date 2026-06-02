662fba403d9c386d2f769365bb2f78c9bb73202d
sarii-css <lesliesaraiberistainp23.cb59@dgeti.sems.gob.mx>
2026-06-02 09:42:35 -0600

    ip/emulador

--- lib/config.dart
+++ lib/config.dart
@@ -1,18 +1,12 @@
 import 'dart:io';
-import '../utils/device_utils.dart';
 
 class Config {
-  static Future<String> get baseUrl async {
-    final isEmulator = await DeviceUtils.isEmulator();
-
+  static String get baseUrl {
     if (Platform.isAndroid) {
-      if (isEmulator) {
-        return "http://10.0.2.2:8080"; 
-      } else {
-        return "http://192.168.0.10:8080"; 
-      }
+      // camiar entre ip (test en celular) & "http://10.0.2.2:8080" (test en emulador)
+      return "http://192.168.0.10:8080";
+    } else {
+      return "http://localhost:8080";
     }
-
-    return "http://localhost:8080";
   }
 }
